2a03d85dd403f70cf36c68743a6fe71d01ef9250
Michael-Coetzee <michaelcoetzee146@gmail.com>
2018-10-06 21:08:32 +0200

    add section to stop you from deploying rovers with the same coordinates

--- basic/mars_rovers.py
+++ basic/mars_rovers.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-## initialise global variables
+# initialise global variables
 # amount of rovers that landed
 rover_count = 2
-# orientation list 
+# orientation list
 orient = ['N', 'E', 'S', 'W']
-# movement of rovers, 1 grid point 
+# movement of rovers, 1 grid point
 move = {'N': (0, 1), 'E': (1, 0), 'S': (0, -1), 'W': (-1, 0)}
 # commands sent to rovers
 instructions = {'L': 'tleft', 'R': 'tright', 'M': 'move'}
@@ -65,12 +65,14 @@ if __name__ == '__main__':
     """
     future improvements
     1. add auto manual functionality, auto: run a input instructions file
-    2. choose amount of rovers to deploy
+    2. manually add the rovers and instructions
+    3. choose amount of rovers to deploy
     """
     # get grid size from input
     xmax, ymax = map(int, input('grid:').split())
     # initialise intersection rovers soon to be positions
     intersection = set([])
+    check_coords = []
     results = []
     # count_a for rover_count for loop
     count_a = 1
@@ -81,21 +83,26 @@ if __name__ == '__main__':
         # get rover coordinates and NESW bearing
         x, y, bearing = input('coordinates for rover %d:' % count_a).split()
         count_a += 1
-        # run class with parameters - store in rover
-        rover = Rover(int(x), int(y), xmax, ymax, bearing, intersection)
-        # iterate over instructions string
-        for i in input('instructions for rover %d:' % count_b):
-            if i not in 'MRL':
-                # exit if not valid instruction
-                print('invalid instruction "%s": use M or R or L - please try again' % i)
-                exit()
-            else:
-                # store and run instructions
-                getattr(rover, instructions[i])()
-        count_b += 1
-        # add nrovers coords to intersection
-        intersection.add((rover.x, rover.y))
-        results.append((rover.x, rover.y, rover.bearing))
+        # check to see that you havent deployed rovers with the same coordinates
+        if [x, y, bearing] not in check_coords:
+            check_coords.append([x, y, bearing])
+            rover = Rover(int(x), int(y), xmax, ymax, bearing, intersection)
+            # iterate over instructions string
+            for i in input('instructions for rover %d:' % count_b):
+                if i not in 'MRL':
+                    # exit if not valid instruction
+                    print('invalid instruction "%s": use M or R or L - please try again' % i)
+                    exit()
+                else:
+                    # store and run instructions
+                    getattr(rover, instructions[i])()
+            count_b += 1
+            # add nrovers coords to intersection
+            intersection.add((rover.x, rover.y))
+            results.append((rover.x, rover.y, rover.bearing))
+        else:
+            print('2 or more of your rovers share the same spot, please try again')
+            exit()
     # print results
     for x, y, z in results:
         print(x, y, z)
